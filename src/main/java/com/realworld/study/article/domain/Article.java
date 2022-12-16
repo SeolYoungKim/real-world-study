7da225c097ec3d935f6b66f0d7b31a01133a6dfa
2jun0 <soo28819@gmail.com>
2022-12-16 22:13:21 +0900

    Feat(Article): Article 엔티티 구현.

---
+++ src/main/java/com/realworld/study/article/domain/Article.java
@@ -0,0 +1,25 @@
+package com.realworld.study.article.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "article")
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 30)
+    private String title;
+
+    @Column(name = "contents", nullable = false)
+    private String contents;
+}
