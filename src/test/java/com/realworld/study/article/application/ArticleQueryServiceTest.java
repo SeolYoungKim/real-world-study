c1422d9b78c954029a25f5c218510e44f20d7e2d
2jun0 <soo28819@gmail.com>
2022-12-20 01:38:45 +0900

    Test(Article): 여러건 조회 기능 테스트 작성.

---
+++ src/test/java/com/realworld/study/article/application/ArticleQueryServiceTest.java
@@ -0,0 +1,41 @@
+package com.realworld.study.article.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.realworld.study.article.domain.Article;
+import com.realworld.study.article.domain.ArticleRepository;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+
+@SpringBootTest
+class ArticleQueryServiceTest {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Autowired
+    ArticleQueryService articleQueryService;
+
+    @DisplayName("게시글 여러건 조회")
+    @Nested
+    class GetArticles {
+
+        @DisplayName("2개를 모두 불러온다.")
+        @Test
+        void getArticles() {
+            Article article1 = articleRepository.save(new Article("article", "description", "body"));
+            Article article2 = articleRepository.save(new Article("article2", "description2", "body2"));
+
+            List<Article> articles = articleQueryService.getArticles(Pageable.ofSize(2)).getContent();
+
+            assertThat(articles.size()).isEqualTo(2);
+            assertThat(articles.get(0)).isEqualTo(article1);
+            assertThat(articles.get(1)).isEqualTo(article2);
+        }
+    }
+}
