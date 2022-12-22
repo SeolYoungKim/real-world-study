7fa748eee6d4c98a22f239f192c0e67f22ea2191
taekwon-dev <taekwon_dev@naver.com>
2022-12-22 10:15:46 +0900

    feat: Member Entity

---
+++ src/main/java/com/realworld/study/Member/domain/Member.java
@@ -0,0 +1,34 @@
+package com.realworld.study.Member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    protected Member() {
+    }
+
+    public Member(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Member(String username, String password, String email) {
+        this(null, username, password, email);
+    }
+}
