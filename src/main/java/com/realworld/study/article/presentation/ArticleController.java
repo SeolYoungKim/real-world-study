f819716fc1e290307d3c890c3d2853aa19d3a7fa
2jun0 <soo28819@gmail.com>
2022-12-22 21:04:04 +0900

    Refactor(Article): 게시글 추가 요청 스펙 변경.

--- src/main/java/com/realworld/study/article/presentation/ArticleController.java
+++ src/main/java/com/realworld/study/article/presentation/ArticleController.java
@@ -1,8 +1,8 @@
 package com.realworld.study.article.presentation;
 
 import com.realworld.study.article.application.ArticleService;
+import com.realworld.study.article.application.dto.ArticleCreateRequest;
 import com.realworld.study.article.domain.Article;
-import com.realworld.study.article.presentation.dto.ArticleCreateRequestWrapper;
 import com.realworld.study.article.presentation.dto.ArticleCreateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +20,8 @@ public class ArticleController {
 
     @PostMapping("/")
     public ResponseEntity<ArticleCreateResponse> createArticle(
-            @RequestBody final ArticleCreateRequestWrapper articleCreateRequestWrapper) {
-        Article article = articleService.createArticle(
-                articleCreateRequestWrapper.getArticleCreateRequest());
+            @RequestBody final ArticleCreateRequest articleCreateRequest) {
+        Article article = articleService.createArticle(articleCreateRequest);
 
         return ResponseEntity.ok(ArticleCreateResponse.of(article));
     }
