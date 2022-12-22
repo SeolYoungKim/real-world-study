764fa2fa1df3bb32c9a21ef8695889a1aadae9df
SeolYoungKim <nasur4da@gmail.com>
2022-12-22 22:41:19 +0900

    refactor(Member): 비밀번호 검증 로직 추가

--- src/main/java/com/realworld/study/user/domain/Member.java
+++ src/main/java/com/realworld/study/user/domain/Member.java
@@ -43,6 +43,7 @@ public class Member extends BaseTimeEntity {
 
     public Member(final String email, final String password, final String memberName,
             final String bio, final String image) {
+        validatePassword(password);
         this.email = new Email(email);
         this.password = password;
         this.memberName = memberName;
@@ -62,5 +63,11 @@ public class Member extends BaseTimeEntity {
         this.email = new Email(email);
         this.bio = bio;  //TODO 없앨 가능성도 고려해서 따로 null 체크 안함
         this.image = image;
+    }
+
+    private void validatePassword(String password) {
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("비밀번호는 8자리 이상 입력해 주세요.");
+        }
     }
 }
