6d4130b537a94ff9afb41c1256cbcf6d167033d0
SeolYoungKim <nasur4da@gmail.com>
2022-12-22 22:08:49 +0900

    feat(currentMember): 현재 유저 조회 기능 구현

--- src/main/java/com/realworld/study/user/application/MemberService.java
+++ src/main/java/com/realworld/study/user/application/MemberService.java
@@ -28,4 +28,15 @@ public class MemberService {
         return new Member(dto.getEmail(), dto.getPassword(), dto.getMemberName(),
                 "", "");
     }
+
+    @Transactional(readOnly = true)
+    public MemberAuthResponse currentMember() {
+        //TODO 인증 객체가 넘어오면 그를 기반으로 멤버를 조회해야 한다.
+        Long memberId = 1L;  // 인증 객체로부터 획득
+        String token = "token";  // 인증 객체로부터 획득
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("없는 회원입니다."));
+
+        return MemberAuthResponse.from(member, token);
+    }
 }
--- src/main/java/com/realworld/study/user/presentation/MemberController.java
+++ src/main/java/com/realworld/study/user/presentation/MemberController.java
@@ -2,9 +2,14 @@ package com.realworld.study.user.presentation;
 
 import com.realworld.study.user.application.MemberService;
 import com.realworld.study.user.application.dto.MemberAuthResponse;
+import com.realworld.study.user.application.dto.MemberProfileResponse;
 import com.realworld.study.user.presentation.dto.MemberSignupRequest;
+import com.realworld.study.user.presentation.dto.MemberUpdateRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +24,10 @@ public class MemberController {
     public MemberAuthResponse signup(@RequestBody MemberSignupRequest memberSignupRequest) {
         return memberService.signup(memberSignupRequest);
     }
+
+    @GetMapping("/member")
+    public MemberAuthResponse currentMember() {
+        return memberService.currentMember();
+    }
+
 }
