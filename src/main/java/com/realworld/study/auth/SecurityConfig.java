9a6815b8be707a6461ecce8be0329661215619f1
SeolYoungKim <nasur4da@gmail.com>
2022-12-28 22:37:52 +0900

    refactor(SecurityConfig): 게시글 조회 관련 엔드포인트 permitAll로 설정

--- src/main/java/com/realworld/study/auth/SecurityConfig.java
+++ src/main/java/com/realworld/study/auth/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -29,7 +30,9 @@ public class SecurityConfig {
                 new CustomAuthenticationProvider(userDetailsService, passwordEncoder()));
 
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/**").permitAll());
+                .requestMatchers("/api/posts").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                .anyRequest().authenticated());
 
         return http.build();
     }
