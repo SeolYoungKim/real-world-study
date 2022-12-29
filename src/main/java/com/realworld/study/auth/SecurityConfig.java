dfef4888eb58a6f84e26e10eef7bcae4905e7274
SeolYoungKim <nasur4da@gmail.com>
2022-12-29 21:55:44 +0900

    refactor(SecurityConfig): @EnableWebSecurity 삭제하고 한번 테스트해봄

--- src/main/java/com/realworld/study/auth/SecurityConfig.java
+++ src/main/java/com/realworld/study/auth/SecurityConfig.java
@@ -8,14 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
