ef7a141d8c2034d850425f9ed2de65e56084a20d
2jun0 <soo28819@gmail.com>
2022-12-29 22:37:22 +0900

    Refactor(User): user의 테이블 명 수정.

--- src/main/java/com/realworld/study/article/domain/User.java
+++ src/main/java/com/realworld/study/article/domain/User.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class User extends BaseEntity {
