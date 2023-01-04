1de7adbeed356ce7ba0035880f37fc73787ccc87
SeolYoungKim <nasur4da@gmail.com>
2023-01-04 15:17:24 +0900

    refactor(CustomLoginSuccessHandler): 토큰을 반환 해주는 전략 변경

--- src/main/java/com/realworld/study/auth/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/realworld/study/auth/handler/CustomLoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.realworld.study.auth.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.realworld.study.auth.handler.dto.AccessTokenResponse;
 import com.realworld.study.auth.jwt.JwtProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,14 +17,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtProvider jwtProvider;
-//    private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
         String accessToken = jwtProvider.accessToken(authentication);
         response.setHeader(HttpHeaders.AUTHORIZATION, accessToken);
-        response.sendRedirect("/api/posts");
-//        objectMapper.writeValue(response.getWriter(), new AccessTokenResponse(accessToken));
+//        response.sendRedirect("/api/posts");  // Redirect 해버릴 경우 받는 측(프론트)에서 토큰을 못잡아낼 것 같음
+        objectMapper.writeValue(response.getWriter(), new AccessTokenResponse(accessToken));
     }
 }
