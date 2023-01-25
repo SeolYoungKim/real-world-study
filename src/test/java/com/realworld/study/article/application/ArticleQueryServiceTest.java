4b915eccdccc04db46ef21ee4c5c530a0e3fe7ba
2jun0 <soo28819@gmail.com>
2023-01-25 11:29:25 +0900

    Test(Article): 여러건 조회 테스트 코드 작성.

--- src/test/java/com/realworld/study/article/application/ArticleQueryServiceTest.java
+++ src/test/java/com/realworld/study/article/application/ArticleQueryServiceTest.java
@@ -7,15 +7,20 @@ import com.realworld.study.article.domain.ArticleRepository;
 import com.realworld.study.user.domain.User;
 import com.realworld.study.user.domain.UserRepository;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class ArticleQueryServiceTest {
+
+    static final String defaultUserEmail = "user@gmail.com";
 
     @Autowired
     ArticleRepository articleRepository;
@@ -25,6 +30,12 @@ class ArticleQueryServiceTest {
     @Autowired
     ArticleQueryService articleQueryService;
 
+    @BeforeEach
+    void insertDefaultUser() {
+        User user = new User(defaultUserEmail, "pw", "user", "bio", null);
+        userRepository.save(user);
+    }
+
     @DisplayName("게시글 여러건 조회")
     @Nested
     class GetArticles {
@@ -32,17 +43,25 @@ class ArticleQueryServiceTest {
         @DisplayName("2개를 모두 불러온다.")
         @Test
         void getArticles() {
-            User author = new User("user@gmail.com", "pw", "user", "bio", null);
-            userRepository.save(author);
-
-            Article article1 = articleRepository.save(new Article("article", "description", "body", author));
-            Article article2 = articleRepository.save(new Article("article2", "description2", "body2", author));
+            Article article1 = createArticle("article1");
+            Article article2 = createArticle("article2");
 
             List<Article> articles = articleQueryService.getArticles(Pageable.ofSize(2)).getContent();
 
             assertThat(articles.size()).isEqualTo(2);
-            assertThat(articles.get(0)).isEqualTo(article1);
-            assertThat(articles.get(1)).isEqualTo(article2);
+            assertThat(articles.get(0)).isEqualTo(article2);
+            assertThat(articles.get(1)).isEqualTo(article1);
         }
+    }
+
+    Article createArticle(String title) {
+        User author = getUser();
+
+        return articleRepository.save(
+                new Article(title, "description", "body", author));
+    }
+
+    User getUser() {
+        return userRepository.findByEmail(defaultUserEmail).orElseThrow();
     }
 }
